641b308d62cd8fecf546204ea99ba936df0d6c19
Cail Borg <cailborg@gmail.com>
2021-02-12 10:05:05 +1100

    added font family utilities and deleted tailwind reference

--- src/TestComponent/TestComponent.tsx
+++ src/TestComponent/TestComponent.tsx
@@ -12,8 +12,8 @@ export const TestComponent: React.FC<TestComponentProps> = ({variant="primary", 
   return (
     <Wrapper>
       <Flow classNames="group">
-        <div className="ring-4 ring-amethyst-600 ring-offset-4 ring-offset-neutral-white rounded-sm transition-transform transform hover:translate-x-1/2 test placeholder-persimmon-100::-webkit-input-placeholder bg-primary px-tiny sm:px-5 m--1" data-variant={variant}>{children}</div>
-        <div className=" test shadow-high border-thick border-eminence-600 hover:border-sky-600 border-indigo-600 p-jumbo hover:text-indigo-600 text-heading-xl group-hover:bg-green leading-heading-xl">{children}</div>
+        <div className="font-heading ring-4 ring-amethyst-600 ring-offset-4 ring-offset-neutral-white rounded-sm transition-transform transform hover:translate-x-1/2 test placeholder-persimmon-100::-webkit-input-placeholder bg-primary px-tiny sm:px-5 m--1" data-variant={variant}>{children}</div>
+        <div className="font-body test shadow-high border-thick border-eminence-600 hover:border-sky-600 border-indigo-600 p-jumbo hover:text-indigo-600 text-heading-xl group-hover:bg-green leading-heading-xl">{children}</div>
       </Flow>
     </Wrapper>
   );
