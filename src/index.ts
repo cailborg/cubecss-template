62fc1fa860bcec6f1bd5bf4b78b07b33e1b564b8
Cail Borg <cailborg@gmail.com>
2021-03-18 14:30:19 +1100

    renamed scss folder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Button }  from "./Button/Button";
 import { TestComponent } from "./TestComponent/TestComponent"
 import { Flow, Wrapper, Splitter, VisuallyHidden }  from "./Utilities/Utilities";
-import  "./scss/styles.scss"
+import  "./_scss/styles.scss"
 
 export { Button, TestComponent, Flow, Wrapper, Splitter, VisuallyHidden };
