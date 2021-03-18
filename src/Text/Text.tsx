59a06511ebbfb71b302ff142b0a68ef502c6e1ea
Cail Borg <cailborg@gmail.com>
2021-03-18 13:19:10 +1100

    cleaned up text css

--- src/Text/Text.tsx
+++ src/Text/Text.tsx
@@ -11,53 +11,55 @@ export interface TextProps {
     className?: string;
 }
 
+const getVariant = (variant: TextProps["variant"]) => {
+  switch (variant) {
+      case "body1":
+          return `font-family: var(--lp-fonts-body-1);
+          font-size: var(--lp-font-size-body-1);
+          line-height: var(--lp-line-height-body-1);
+          letter-spacing: var(--lp-letter-spacing-body-1)`;
+      case "body2":
+          return `font-family: var(--lp-fonts-body-2);
+          font-size: var(--lp-font-size-body-2);
+          line-height: var(--lp-line-height-body-2);
+          letter-spacing: var(--lp-letter-spacing-body-2);`
+      case "body3":
+          return `font-family: var(--lp-fonts-body-3);
+          font-size: var(--lp-font-size-body-3);
+          line-height: var(--lp-line-height-body-3);
+          letter-spacing: var(--lp-letter-spacing-body-3);`;
+      case "caption":
+          return `font-family: var(--lp-fonts-caption);
+          font-size: var(--lp-font-size-caption);
+          line-height: var(--lp-line-height-caption);
+          letter-spacing: var(--lp-letter-spacing-caption);`;
+      case "overline":
+          return `font-family: var(--lp-fonts-overline);
+          font-size: var(--lp-font-size-overline);
+
+          line-height: var(--lp-line-height-overline);
+          letter-spacing: var(--lp-letter-spacing-overline);
+          text-transform: uppercase`;
+      default:
+          return `font-family: inherit;
+          font-size: inherit;
+          line-height: inherit;
+          letter-spacing: inherit;`;
+  }
+};
+
 export const Text: React.FC<TextProps> = ({
-    variant = "inherit",
+    variant,
     children,
     tagName = "span",
-    alignment = "inherit",
+    alignment,
     id,
     className,
     isEmphasised
 }) => {
-    const getVariant = (variant: string) => {
-        switch (variant) {
-            case "body1":
-                return `font-family: var(--lp-fonts-body-1);
-                font-size: var(--lp-font-size-body-1);
-                line-height: var(--lp-line-height-body-1);
-                letter-spacing: var(--lp-letter-spacing-body-1)`;
-            case "body2":
-                return `font-family: var(--lp-fonts-body-2);
-                font-size: var(--lp-font-size-body-2);
-
-                line-height: var(--lp-line-height-body-2);
-                letter-spacing: var(--lp-letter-spacing-body-2);`
-            case "body3":
-                return `font-family: var(--lp-fonts-body-3);
-                font-size: var(--lp-font-size-body-3);
-
-                line-height: var(--lp-line-height-body-3);
-                letter-spacing: var(--lp-letter-spacing-body-3);`;
-            case "caption":
-                return `font-family: var(--lp-fonts-caption);
-                font-size: var(--lp-font-size-caption);
-
-                line-height: var(--lp-line-height-caption);
-                letter-spacing: var(--lp-letter-spacing-caption);`;
-            case "overline":
-                return `font-family: var(--lp-fonts-overline);
-                font-size: var(--lp-font-size-overline);
-
-                line-height: var(--lp-line-height-overline);
-                letter-spacing: var(--lp-letter-spacing-overline);
-                text-transform: uppercase`;
-            default:
-                return `body1 styles`;
-        }
-    };
+    
     const TextTag = styled.span`
-        text-align: ${alignment};
+        text-align: ${alignment ?? "inherit"};
         ${getVariant(variant)};
         font-weight: ${isEmphasised ? '700' : '400'};
     `;
