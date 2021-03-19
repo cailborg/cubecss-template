50b0183c1a82c78abcee08ff78ba9c0e43d328f9
Cail Borg <cailborg@gmail.com>
2021-03-19 13:28:50 +1100

    removed test component and hid cwitch control

--- src/Switch/Switch.stories.tsx
+++ src/Switch/Switch.stories.tsx
@@ -5,10 +5,15 @@ import { Switch } from "./Switch";
 export default {
     title: "Components/Switch",
     component: Switch,
+    argTypes:{
+      checked:{
+        control:false
+      }
+    }
 } as Meta;
 
 export const Standard = (args: any) => {
     const [checked, setChecked] = useState(false);
     return <Switch {...args} checked={checked} onChange={setChecked} />;
 };
-Standard.args = { label: "Label Text" };
+// Standard.args = { label: "Label Text" };
