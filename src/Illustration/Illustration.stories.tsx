9af0d49d8d3bdb1925f93293f859226fb4287302
Cail Borg <cailborg@gmail.com>
2021-04-01 10:45:33 +1100

    clean up story file

--- src/Illustration/Illustration.stories.tsx
+++ src/Illustration/Illustration.stories.tsx
@@ -4,10 +4,6 @@ import { Illustration, IllustrationProps } from "./Illustration";
 export default {
   title: "Components/Illustration",
   component: Illustration,
-  // argTypes: {
-  //   title: { control: "text" },
-  //   value: { control: "text" },
-  // }
 } as Meta;
 
 export const Horizontal: Story<IllustrationProps> = (args) => <Illustration {...args}></Illustration>;
