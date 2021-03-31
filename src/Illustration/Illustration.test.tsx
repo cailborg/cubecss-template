b57cded4f961928cc6361e0bd0b8841545c1b36d
Cail Borg <cailborg@gmail.com>
2021-04-01 10:44:30 +1100

    update snapshots

--- src/Illustration/Illustration.test.tsx
+++ src/Illustration/Illustration.test.tsx
@@ -3,6 +3,6 @@ import { Illustration } from "./Illustration";
 import renderer from "react-test-renderer";
 
 it("renders correctly", () => {
-  const tree = renderer.create(<Illustration name="Purchases" title="Purchases"></Illustration>).toJSON();
+  const tree = renderer.create(<Illustration size="large" name="Purchases" title="Purchases"></Illustration>).toJSON();
   expect(tree).toMatchSnapshot();
 });
